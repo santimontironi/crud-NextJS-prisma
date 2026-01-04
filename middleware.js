53ac814bd0383f642c12e0150d4510a370155c9c
santimontironi <santiimontironi@gmail.com>
2026-01-04 10:47:25 -0300

    archivo middleware.js agregado

---
+++ middleware.js
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.NEXT_PUBLIC_JWT_SECRET;
+
+export function middleware(request) {
+  const token = request.cookies.get("token")?.value;
+
+  const protectedPaths = ["/new", "/tasks"];
+
+  const { pathname } = request.nextUrl;
+
+  const isProtected = protectedPaths.some(path =>
+    pathname.startsWith(path)
+  );
+
+  if (!isProtected) return NextResponse.next();
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  try {
+    jwt.verify(token, JWT_SECRET);
+    return NextResponse.next();
+  } catch {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+}
